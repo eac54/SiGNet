fa0ff9fc01a744eb70a61ae4a393beea65ae77ee
eac54 <eac54@users.noreply.github.com>
2017-01-23 10:49:49 +0000

    Add files via upload

---
+++ CreateNetwork.java
@@ -0,0 +1,58 @@
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNode;
+import org.cytoscape.view.vizmap.VisualStyle;
+
+
+public class CreateNetwork {
+	VisualStyle style;
+
+	
+	
+	public VisualStyle getStyle() {
+		return style;
+	}
+	
+	/**
+	 * CONSTRUCTOR
+	 * @param Number of nodes
+	 * @param Number of edges
+	 
+	 */
+	public CreateNetwork( Double TotNodes, Double TotEdges) {
+		// Create an empty network
+			
+				// For i in number of nodes needed {
+				// Create correct number of named nodes
+				
+				//... }
+				
+				
+				// If number of edges was specified {
+				// For i in number of edges {
+				// randomly add edges
+				//...}
+				// else {
+				// randomly add edge
+				//calculate average degree of overall network
+				// if ave degree > target {
+				// stop
+				//}
+				//...}
+						
+				
+				
+				
+				// Set the variable destroyNetwork to true, the following code will destroy a network
+				
+				
+					// Destroy it
+				
+			
+				// New Jframe displaying network stats and user defined inputs
+		
+		
+	}
+
+}
+
+
